8824094fe539d29e58832dc73b2a011b76398c15
Miguel <miguelasafedev@gmail.com>
2025-12-29 00:47:32 -0500

    final

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,17 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
         Curso curso1 = new Curso();
-        curso1.setTitulo("Curso Java");
-        curso1.setDescricao("Curso direcionado para Back-End em Java");
+        curso1.setTitulo("Curso LPA - Java");
+        curso1.setDescricao("Curso direcionado logica de programacao em Java");
         curso1.setCargaHoraria(8);
 
         Curso curso2 = new Curso();
-        curso2.setTitulo("Curso Python");
-        curso2.setDescricao("Curso direcionado para Back-End em Python");
+        curso2.setTitulo("Curso POO - Java");
+        curso2.setDescricao("Curso direcionado para programacao orientada a objetos em Java");
         curso2.setCargaHoraria(5);
 
         Curso curso3 = new Curso();
@@ -22,8 +20,8 @@ public class Main {
         curso3.setCargaHoraria(10);
 
         Mentoria mentoria1 = new Mentoria();
-        mentoria1.setTitulo("Mentoria React");
-        mentoria1.setDescricao("Mentoria direcionada para framework de Front-End");
+        mentoria1.setTitulo("Mentoria Spring Boot");
+        mentoria1.setDescricao("Mentoria direcionada para framework de Back-End em Java");
         mentoria1.setData(LocalDate.now());
 
         Mentoria mentoria2 = new Mentoria();
@@ -31,10 +29,41 @@ public class Main {
         mentoria2.setDescricao("Mentoria direcionada para banco de dados relacionais com MySql");
         mentoria2.setData(LocalDate.now());
 
-        System.out.println(curso1);
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(curso3);
         System.out.println(mentoria1);
-        System.out.println(mentoria2);
+        System.out.println(mentoria2);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Bootcamp focado em desenvolvimento Back-End com java");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devClaudio = new Dev();
+        devClaudio.setNome("Claudio");
+        devClaudio.inscreverBootcamp(bootcamp);
+        System.out.println("Claudio - Conteudos Inscritos: " + devClaudio.getConteudosInscritos());
+        devClaudio.progredir();
+        devClaudio.progredir();
+        System.out.println("Claudio - Conteudos Inscritos: " + devClaudio.getConteudosInscritos());
+        System.out.println("Claudio - Conteudos Concluidos: " + devClaudio.getConteudosConcluidos());
+        System.out.println("XP: " + devClaudio.calcularTotalXp());
+
+        System.out.println("------------------------");
+
+        Dev devBianca = new Dev();
+        devBianca.setNome("Bianca ");
+        devBianca.inscreverBootcamp(bootcamp);
+        System.out.println("Bianca - Conteudos Inscritos: " + devBianca.getConteudosInscritos());
+        devBianca.progredir();
+        System.out.println("Bianca - Conteudos Inscritos: " + devBianca.getConteudosInscritos());
+        System.out.println("Bianca - Conteudos Concluidos: " + devBianca.getConteudosConcluidos());
+        System.out.println(devBianca.calcularTotalXp());
+
     }
+
 }
--- src/br/com/dio/desafio/dominio/Bootcamp.java
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -7,19 +7,19 @@ import java.util.Objects;
 import java.util.Set;
 
 public class Bootcamp {
-    private String titulo;
+    private String nome;
     private String descricao;
     private final LocalDate dataInicio = LocalDate.now();
     private final LocalDate dataFinal = dataInicio.plusDays(45);
     private Set<Dev> devInscritos = new HashSet<>();
     private Set<Conteudo> conteudos = new LinkedHashSet<>();
 
-    public String getTitulo() {
-        return titulo;
+    public String getNome() {
+        return nome;
     }
 
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
+    public void setNome(String titulo) {
+        this.nome = titulo;
     }
 
     public String getDescricao() {
@@ -58,11 +58,11 @@ public class Bootcamp {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Bootcamp bootcamp = (Bootcamp) o;
-        return Objects.equals(titulo, bootcamp.titulo) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicio, bootcamp.dataInicio) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devInscritos, bootcamp.devInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
+        return Objects.equals(nome, bootcamp.nome) && Objects.equals(descricao, bootcamp.descricao) && Objects.equals(dataInicio, bootcamp.dataInicio) && Objects.equals(dataFinal, bootcamp.dataFinal) && Objects.equals(devInscritos, bootcamp.devInscritos) && Objects.equals(conteudos, bootcamp.conteudos);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(titulo, descricao, dataInicio, dataFinal, devInscritos, conteudos);
+        return Objects.hash(nome, descricao, dataInicio, dataFinal, devInscritos, conteudos);
     }
 }
