f6450d767689f7f38527975168f28ed8930e6eba
Miguel <miguelasafedev@gmail.com>
2025-12-28 20:27:13 -0500

    add classes curso e mentoria

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,33 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso direcionado para Back-End em Java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Python");
+        curso2.setDescricao("Curso direcionado para Back-End em Python");
+        curso2.setCargaHoraria(5);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria React");
+        mentoria1.setDescricao("Mentoria direcionada para framework de Front-End");
+        mentoria1.setData(LocalDate.now());
+
+        Mentoria mentoria2 = new Mentoria();
+        mentoria2.setTitulo("Mentoria My SQL");
+        mentoria2.setDescricao("Mentoria direcionada para banco de dados relacionais com MySql");
+        mentoria2.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
     }
 }
